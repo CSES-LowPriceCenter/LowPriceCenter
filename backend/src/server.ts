b855dcdfc99d1eb41ba9cdf317ab832c1c603a58
nicholas-ngyn <nicholas.ngyn@gmail.com>
2025-04-27 17:30:06 -0700

    added build and start to backend package.json

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -7,13 +7,19 @@ import mongoose from "mongoose";
 import app from "src/app";
 import env from "src/util/validateEnv";
 
-const PORT = env.PORT;
+const PORT = env.PORT || 5000;
 const MONGODB_URI = env.MONGODB_URI;
 
-app.listen(PORT, "0.0.0.0", () => {
-  console.log(`Server running on port ${PORT}`);
-  mongoose.connect(MONGODB_URI)
-    .then(() => console.log("Mongoose connected!"))
-    .catch(err => console.error("MongoDB connection error:", err));
-});
+console.log("Connecting to MongoDB...");
+mongoose
+  .connect(MONGODB_URI)
+  .then(() => {
+    console.log("Mongoose connected!");
+    app.listen(PORT, "0.0.0.0", () => {
+      console.log(`Server running on port ${PORT}`);
+    });
+  })
+  .catch((error) => {
+    console.error("MongoDB connection error:", error);
+  });
 
